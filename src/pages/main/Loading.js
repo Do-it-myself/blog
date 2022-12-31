a010ea9f3fad7e184b62c5c2927db9707fb17bb6
Do-it-myself <doitmyself111.engineering@gmail.com>
2022-12-31 14:36:56 +0800

    loading

--- src/pages/main/Loading.js
+++ src/pages/main/Loading.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 export default function Loading() {
+  const homeIsNarrow = useMediaQuery({ query: "(max-aspect-ratio: 4/5)" });
+
   return (
-    <div>Loading</div>
-  )
+    <div className="loading">
+      <div className={homeIsNarrow ? "loadingNarrow" : "loadingWide"}>
+        <img
+          src={require("../../assets/images/loading.gif")}
+          alt="loading"
+          className="image"
+        />
+      </div>
+    </div>
+  );
 }
